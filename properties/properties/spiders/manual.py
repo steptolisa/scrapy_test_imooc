7ebd9eab89186a976b837f7d7eb623ec619d146f
dht <gwyylisa1234@163.com>
2018-12-06 14:24:41 +0800

    更新

--- properties/properties/spiders/manual.py
+++ properties/properties/spiders/manual.py
@@ -10,46 +10,27 @@ from urllib import parse
 
 class BasicSpider(scrapy.Spider):
     name = 'manual'
-    allowed_domains = ['web']
-    start_urls = ['https://bj.5i5j.com/ershoufang/?pmf_group=baidu&pmf_medium=cpc&pmf_plan=%E4%BA%8C%E6%89%8B%E6%88%BF%E9%80%9A%E7%94%A8%E8%AF%8D&pmf_unit=%E4%BA%8C%E6%89%8B%E6%88%BF%E6%88%BF%E6%BA%90&pmf_keyword=%E4%BA%8C%E6%89%8B%E6%88%BF&pmf_account=36&pmf_id=8807669335']
+    allowed_domains = ['www.imooc.com']
+    # start_urls = ['https://bj.5i5j.com/ershoufang/?pmf_group=baidu&pmf_medium=cpc&pmf_plan=%E4%BA%8C%E6%89%8B%E6%88%BF%E9%80%9A%E7%94%A8%E8%AF%8D&pmf_unit=%E4%BA%8C%E6%89%8B%E6%88%BF%E6%88%BF%E6%BA%90&pmf_keyword=%E4%BA%8C%E6%89%8B%E6%88%BF&pmf_account=36&pmf_id=8807669335']
+    start_urls = ["http://www.imooc.com/course/list"]
 
     def parse(self, response):
 
-        # Get the next index URLs and yield Requests
-        next_selector = response.xpath('/html/body//a[@class="cPage"]/@href')
+        # url 跟进
+        # 获取下一页url信息
+        next_selector = response.xpath("//a[contains(text(),'下一页')]/@href")
         for url in next_selector.extract():
             yield Request(parse.urljoin(response.url, url))
-
-        # get item URLs and yield Requests
-        item_selector = response.xpath('/html/body//h3/a/@href')
+        item_selector = response.xpath('/html/body//div[@class="course-card-container"]/a[@target="_blank"]//@href')
         for url in item_selector.extract():
             yield Request(parse.urljoin(response.url, url), callback=self.parse_item)
 
 
-
     def parse_item(self, response):
-        """ This is a function parses a scrapy.org page.
 
-        @url https://scrapy.org/
-        @returns items 1
-        @scrapes title url spider date server
-        """
+        item = PropertiesItem()
 
-        # self.log("title: %s" % response.xpath('title//text()').extract_first())
-        l = ItemLoader(item=PropertiesItem(), response=response)
-        # item['title'] = response.xpath('//title/text()').extract_first()
+        item['url'] = response.url
+        item['title'] = response.xpath('/html/head/title/text()').extract()[0]
 
-
-        # l.add_xpath('title', '/html/body/div[1]/div/div[1]/a/text()')
-        # 添加python计算得出的单个值， 而不是xpath，css 表达式
-        l.add_value('spider', self.name)
-        l.add_value('url', response.url)
-        l.add_value('date', datetime.datetime.now())
-        l.add_value('server', socket.gethostname())
-
-        # {'title': ['Scrapy | A Fast and Powerful Scraping and Web Crawling Framework'],
-         # 'url': ['https://scrapy.org']}
-
-
-
-        return l.load_item()
+        yield item
